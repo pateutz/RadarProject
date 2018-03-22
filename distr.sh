9ee4e9a7cea81837add8acd2e5e90b6e1507d29d
Andrei <pateutz@smallhosting.ro>
2018-03-22 21:10:31 +0200

    Update distr.sh

--- distr.sh
+++ distr.sh
@@ -1,12 +1,11 @@
-  GNU nano 2.5.3              File: distr.sh                                    
-
-# i tested only on ubunt bcs i dont have centos right now but i will improve so$
+                                
+# i tested only on ubuntu bcs i dont have centos right now but i will improve soon
 YUM_CMD=$(yum)
 APT_GET_CMD=$(apt-get)
 
  if [[ ! -z $YUM_CMD ]]; then
-    sudo yum update 
-  #  sudo yum install jdk9 # (i dont have centos for testing but i will improve$
+  #  sudo yum update # (testing after centos will be done xD) 
+  #  sudo yum install jdk9 # (testing after centos will be done xD)
   
  elif [[ ! -z $APT_GET_CMD ]]; then
     sudo add-apt-repository ppa:webupd8team/java
